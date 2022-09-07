625767e6d11e5a82d393559850aed98cba13d59a
Viet-Uy <vietuydo@gmail.com>
2022-09-07 10:54:45 +0200

    Ny

--- Hello.java
+++ Hello.java
@@ -2,6 +2,14 @@ public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        String helloWorld[] = {"H", "e", "l", "l", "o"};
+        for(int i = 0; i < helloWorld.length; i++){
+
+
+            String str = helloWorld[i];
+            System.out.println(str);
+            
+        }
+        
     }
 }
